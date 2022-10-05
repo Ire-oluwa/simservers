7b38613e10e67dd95d3b26b3340158e199c432ba
Orodiji Ireoluwa <orodiji.ireoluwa@gmail.com>
2022-10-05 15:10:54 +0100

    the final splash screen

---
+++ lib/sceens/splash/final_splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FinalSplashScreen extends StatefulWidget {
+  const FinalSplashScreen({Key? key}) : super(key: key);
+  static const String id = "final splash screen";
+
+  @override
+  State<FinalSplashScreen> createState() => _FinalSplashScreenState();
+}
+
+class _FinalSplashScreenState extends State<FinalSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
